4c3a0f7b4d55686358286e4cc13df6f2ccf51330
Prasad750 <138876806+Prasad750@users.noreply.github.com>
2023-11-11 20:21:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 //your JS code here. If required.
 function functionName()
 {
-    alert("functionName");
+    alert(arguments.callee.name);
 }
-
+functionName();
 function AccioJob()
 {
-    alert("AccioJob");
+    alert(AccioJob.name);
 }
-
-// functionName();
-// AccioJob();
+AccioJob();
